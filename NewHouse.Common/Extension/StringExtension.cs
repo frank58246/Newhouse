75fd15b1670d3d088b2e97a821f25c79d50afdbb
Frank <Frank@DESKTOP-FA8EJCS>
2021-01-24 11:29:46 +0800

    新增字串解析方法

--- NewHouse.Common/Extension/StringExtension.cs
+++ NewHouse.Common/Extension/StringExtension.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace NewHouse.Common.Extension
 {
@@ -12,6 +13,26 @@ namespace NewHouse.Common.Extension
         public static bool IsNullOrEmpty(this string s)
         {
             return s is null || s.Equals(string.Empty);
+        }
+
+        public static List<int> ToIntList(this string s)
+        {
+            var intList = new List<int>();
+            if (s.IsNullOrEmpty())
+            {
+                return intList;
+            }
+
+            var matches = Regex.Split(s, @"\D+");
+            foreach (var match in matches)
+            {
+                if (int.TryParse(match, out var number))
+                {
+                    intList.Add(number);
+                }
+            }
+
+            return intList;
         }
     }
 }
---
+++ NewHouse.CommonTests/Extension/StringExtensionTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NewHouse.Common.Extension;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewHouse.Common.Extension.Tests
+{
+    [TestClass()]
+    public class StringExtensionTests
+    {
+        [TestMethod()]
+        public void ToIntListTest_輸入單個數字_應輸出單筆數字()
+        {
+            // Arrange
+            var input = "1000萬";
+            var expected = new List<int>() { 1000 };
+
+            // Act
+            var actual = input.ToIntList();
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void ToIntListTest_輸入多筆數字_使用dash分隔_應輸出多筆數字()
+        {
+            // Arrange
+            var input = "1000-3000萬";
+            var expected = new List<int>() { 1000, 3000 };
+
+            // Act
+            var actual = input.ToIntList();
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void ToIntListTest_輸入多筆數字字串_使用波浪號分隔_應輸出多筆數字()
+        {
+            // Arrange
+            var input = "1000~3000萬";
+            var expected = new List<int>() { 1000, 3000 };
+
+            // Act
+            var actual = input.ToIntList();
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}
